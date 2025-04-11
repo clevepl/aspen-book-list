802979dd56463379a3c203b62c75cad49dae62dd
Will Skora <175735853+skora-cpl@users.noreply.github.com>
2025-04-11 09:28:05 -0400

    lint, add the call again for fetching the listID attribute

--- src/aspen-book-list/render.php
+++ src/aspen-book-list/render.php
@@ -3,6 +3,16 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
+/**
+ * cpl markup
+ *
+ * @package Cpl // TODO update everywhere else
+ *
+ * @var array    $attributes         Block attributes.
+ * @var string   $content            Block content.
+ * @var WP_Block $block              Block instance.
+ * @var array    $context    Block context.
+ */
 
 // function get_api_key(): string {
 // return defined( 'GOOGLE_PLACES_API_KEY' ) ? constant( 'GOOGLE_PLACES_API_KEY' ) : '';
@@ -10,7 +20,12 @@
 
 // TODO: MAKE THIS OOP, (ARE YOU DOWN WITH OOP, YEAH YOU KNOW ME?!)
 
+$list_id = isset( $attributes['listID'] ) ? $attributes['listID'] : '';
 
+// explicitly cast as integer
+settype( $list_id, 'integer' );
+
+// reminder: ASPEN_API_AUTHORIZATION_TOKEN is defined in wp-config.php
 $special_header_args = [
 	'headers' => [
 		'X-Custom-SPECIAL' => constant( 'ASPEN_API_AUTHORIZATION_TOKEN' ),
@@ -22,7 +37,7 @@ $special_header_args = [
 $home_url = 'https://search.cpl.org';
 
 
-$teh_request = wp_remote_get( 'https://search.cpl.org/API/ListAPI?method=getListTitles&id=79054', $special_header_args );
+$teh_request = wp_remote_get( 'https://search.cpl.org/API/ListAPI?method=getListTitles&id=' . $list_id, $special_header_args );
 
 if ( is_wp_error( $teh_request ) ) {
 	return false; // Bail early
@@ -30,20 +45,18 @@ if ( is_wp_error( $teh_request ) ) {
 
 // if headers response code is 403 - then bail and return; your API key is not authorized or there's a connection error with Aspen
 // result-> in an array, titles
-// however, an array is not thrown even though  echo $teh_request['response']['code']; will turn 403.
+// however, an error is not thrown even when echo $teh_request['response']['code']; will turn 403.
+// TODO
 if ( $teh_request['response']['code'] === 403 ) {
 	return new WP_Error( 'Bad response code, 403', 'Falling and cant get up' );
 }
 
 $body = wp_remote_retrieve_body( $teh_request );
 
-$teh_data = json_decode( $body, true ); // true for an array
-
+$teh_data = json_decode( $body, true );
 if ( ! empty( $teh_data ) ) {
 	?>
 <p <?php echo get_block_wrapper_attributes(); ?>> keep this for now, so i don't forget about block_wrapper_attributes </p>
-
-
 
 <div class="master-container">
 
@@ -62,13 +75,7 @@ if ( ! empty( $teh_data ) ) {
 
 		?>
 
-<?php /*
-	<pre>
-		<?php print_r($my_array) ?>
-	</pre>
-*/ ?>
-
-	<div class="media-object-container">
+<div class="media-object-container">
 
 	<div class="media-object__media">
 	<img alt="cover for <?php echo esc_html( $title ); ?>" src="<?php echo esc_url( $home_url . $image_small, 'aspen-book-list' ); ?>" alt="Balloons">
@@ -76,10 +83,10 @@ if ( ! empty( $teh_data ) ) {
 	<div class="media-object__object">
 
 		<h3 class="cpl-flex--mini__item-name"><?php echo esc_html( $title ); ?></h3>
-	
+
 		<?php if ( ! empty( $author ) ) : ?>
-    <span class="cpl-flex--mini__item-date">By: <?php echo esc_html( $author ); ?></span>
-		<?php endif; ?>	
+	<span class="cpl-flex--mini__item-date">By: <?php echo esc_html( $author ); ?></span>
+		<?php endif; ?>
 
 		<div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
 			<div class="wp-block-button">
